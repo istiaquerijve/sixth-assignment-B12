40896344d78d6231f7951a6446116382a1d54404
istiaquerijve <istiaquerijve@gmail.com>
2025-09-08 11:10:14 +0600

    tenth commit

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@ const loadTreeCard = (id) => {
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
+        removeActive();
         const clickBtn = document.getElementById(`card-btn-${id}`)
         clickBtn.classList.add("active")
 
@@ -39,7 +40,7 @@ const displayTreeCard = (cards) => {
             <div class="bg-white h-full w-66 rounded-lg">
                   <div class="p-6">
                     <img class="rounded-lg" src="${card.image}" alt="">
-                    <h1 class="font-bold text-2xl mt-2">${card.name}</h1>
+                    <h1 onclick="my_modal_1.showModal()" class="font-bold text-2xl mt-2">${card.name}</h1>
                     <p class="font-semibold text-gray-600 mt-2">${card.description}</p>
                     <div class="mt-2 flex justify-between">
                       <button class="bg-green-200 text-green-700 border-none rounded-2xl w-30">${card.category}</button>
@@ -63,8 +64,7 @@ const displayTree = (trees) => {
         
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button id="card-btn-${tree.id}" onclick = "loadTreeCard(${tree.id})" class="w-60 border-none bg-sky-100 text-gray-500 
-            text-2xl mt-5 hover:bg-green-600 hover:text-white rounded-xl card-btn">
+            <button id="card-btn-${tree.id}" onclick = "loadTreeCard(${tree.id})" class="card-btn w-60 border-none bg-sky-100 text-gray-500            text-2xl mt-5 hover:bg-green-600 hover:text-white rounded-xl">
             ${tree.category_name}</button>
 
         `;
